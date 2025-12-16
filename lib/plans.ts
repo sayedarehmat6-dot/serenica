2a6f9ce8cf19f11ffb52233d2f70b91fc828d330
sayedarehmat6-dot <sayedarehmat6@gmail.com>
2025-12-16 13:30:06 +0500

    Create plans.ts

---
+++ lib/plans.ts
@@ -0,0 +1,5 @@
+export const PLANS = {
+FREE: { name: 'Free', maxJobs: 3, price: 0 },
+PRO: { name: 'Professional', maxJobs: 100, price: 99 },
+ENTERPRISE: { name: 'Enterprise', maxJobs: Infinity, price: null }
+}
