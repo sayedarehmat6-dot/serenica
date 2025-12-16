bb9873ac92abb67f162b21a0a6d5c2a8ba14ee88
sayedarehmat6-dot <sayedarehmat6@gmail.com>
2025-12-16 14:46:36 +0500

    Create page.tsx

---
+++ app/dashboard/pharma/page.tsx
@@ -0,0 +1,13 @@
+import VariantTable from '@/components/variant-table'
+import GraphView from '@/components/graph-view'
+
+
+export default function PharmaDashboard() {
+return (
+<div>
+<h2 className="text-3xl font-bold text-cyan-400 mb-6">Pharma Dashboard</h2>
+<VariantTable />
+<GraphView />
+</div>
+)
+}
